d276b8fcc878f1b436468121e6ba83f9a26097af
Michael <liumichael101@gmail.com>
2022-04-11 17:39:45 -0700

    declare some js variables

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var searchButtonEl = document.querySelector('#search-button');
+var searchCityEl = document.querySelector('#search-city');
+var historyEl = document.querySelector('#history');
+var apiKey = "4b9b6edec9e1e68028e9e58e3975e5a1";
+var forecastEl = document.querySelector('#forecast');
+
+
